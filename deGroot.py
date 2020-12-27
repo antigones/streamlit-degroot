45fdc94797901372888e70f34e78e948076ab8ab
antigones <>
2020-12-27 23:58:17 +0100

    limit definition adjusted

--- deGroot.py
+++ deGroot.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-
+import sys
 
 def deGroot(environment, beliefs):
     i = 0
@@ -8,7 +8,8 @@ def deGroot(environment, beliefs):
     while (consensus == False and i < 1000):
         old_beliefs = beliefs
         beliefs = environment @ beliefs
-        if (np.all(old_beliefs - beliefs == np.zeros(len(beliefs)))):
+        #if (np.all(old_beliefs - beliefs == np.zeros(len(beliefs)))):
+        if (np.all(old_beliefs - beliefs < np.full(len(beliefs),sys.float_info.epsilon))):
             consensus = True
         i = i + 1
     return beliefs
@@ -21,7 +22,8 @@ def deGrootSteps(environment, beliefs):
     while (consensus == False and i < 1000):
         old_beliefs = beliefs
         beliefs = environment @ beliefs
-        if (np.all(old_beliefs - beliefs == np.zeros(len(beliefs)))):
+        #if (np.all(old_beliefs - beliefs == np.zeros(len(beliefs)))):
+        if (np.all(old_beliefs - beliefs < np.full(len(beliefs),sys.float_info.epsilon))):
             consensus = True
         i = i + 1
         steps.append(beliefs)
